83fbd68dabb4c7c442fe71da1d2923e323a919f8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-30 12:02:09 +0100

    Add project files.

---
+++ Mistero/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryMistero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = Mistero.Mistero6(10,8);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
